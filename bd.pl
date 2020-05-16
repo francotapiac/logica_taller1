ba88b6fb84f7002f6969df0714ae7c73e8fdab59
Matias <matias.coronado@usach.cl>
2020-05-16 17:59:37 -0400

    eliminación de comentarios y funciones inútiles

--- bd.pl
+++ bd.pl
@@ -6,9 +6,9 @@ sintoma('no muerte').
 enfermedad('resfrio comun').
 enfermedad('resfrio mortal').
 
-poblacionRiesgo(adulto)
-poblacionRiesgo(infante)
-poblacionRiesgo('adulto mayor')
+poblacionRiesgo(adulto).
+poblacionRiesgo(infante).
+poblacionRiesgo('adulto mayor').
 
 relacion(enfermedad('resfrio comun'),sintoma(tos)).
 relacion(enfermedad('resfrio comun'),sintoma(fiebre)).
@@ -17,10 +17,13 @@ relacion(enfermedad('resfrio comun'),sintoma('no muerte')).
 relacion(enfermedad('resfrio mortal'),sintoma(tos)).
 relacion(enfermedad('resfrio mortal'),sintoma(fiebre)).
 relacion(enfermedad('resfrio mortal'),sintoma(escalofrios)).
+relacion(enfermedad('resfrio mortal'),sintoma(muerte)).
 
-significativa(enfermedad('resfrio mortal'),sintoma(muerte))
-significativa(enfermedad('resfrio comun'),sintoma('no muerte'))
+significativa(enfermedad('resfrio mortal'),sintoma(muerte)).
+significativa(enfermedad('resfrio comun'),sintoma('no muerte')).
 
-tratamiento(enfermedad('resfrio comun'),poblacionRiesgo(infante),'Ahhh se morira el cabro chico')
-tratamiento(enfermedad('resfrio comun'),poblacionRiesgo(adulto),'Ahhh se morira el adulto')
-tratamiento(enfermedad('resfrio comun'),poblacionRiesgo('adulto mayor'),'Ahhh se morira el adulto mayor')
+tratamiento(enfermedad('resfrio comun'),poblacionRiesgo(infante),'Ahhh se morira el cabro chico').
+tratamiento(enfermedad('resfrio comun'),poblacionRiesgo(adulto),'Ahhh se morira el adulto').
+tratamiento(enfermedad('resfrio comun'),poblacionRiesgo('adulto mayor'),'Ahhh se morira el adulto mayor').
+
+
--- codigo.py
+++ codigo.py
@@ -1,31 +1,12 @@
 from pyswip import Prolog
 from pyswip import Functor
 
-
 prolog = Prolog()
-
 prolog.consult("bd.pl")
-
-#Serie de preguntas.
-#   No cambian su orden, ni cantidad
-#   Siempre las mismas
-
-#Usted tiene fiebre
-#   Si
-#Usted tiene escalofrios
-#   Si
-
-
-#prolog.assertz("que_enfermedad([],10)")
-#prolog.assertz("que_enfermedad([A|B],X):- que_enfermedad(B,X)")
-
-prolog.assertz("primero(X,Y):- X = [Z|_], Y = Z")
-
 
 prolog.assertz("que_enfermedad([], [], E)")
 prolog.assertz("que_enfermedad([], [N|Ns], E):- not(relacion(enfermedad(E), sintoma(N))), que_enfermedad([],Ns,E)")
 prolog.assertz("que_enfermedad([X|Xs],N,E):-    relacion(enfermedad(E), sintoma(X)), que_enfermedad(Xs,N,E)")
-
 
 
 def obtenerEnefermedades(lista):
@@ -34,39 +15,5 @@ def obtenerEnefermedades(lista):
         lista_aux.append(lista[i]["X"])
     return lista_aux
 
-lista = list(prolog.query("que_enfermedad([tos,fiebre],[],X)"))
+lista = list(prolog.query("que_enfermedad([tos,fiebre,'no muerte'],[],X)"))
 print(obtenerEnefermedades(lista))
-
-#FUNCION DE INTERNET PARA TRASPASAR ATOM'S
-#   error de version de pyswip
-def format_value(value):
-    output = ""
-    if isinstance(value, list):
-        output = "[ " + ", ".join([format_value(val) for val in value]) + " ]"
-    elif isinstance(value, Functor) and value.arity == 2:
-        output = "{0}{1}{2}".format(value.args[0], value.name, value.args[1])
-    else:
-        output = "{}".format(value)
-
-    return output
-
-
-def format_result(result):
-    result = list(result)
-
-    if len(result) == 0:
-        return "false."
-
-    if len(result) == 1 and len(result[0]) == 0:
-        return "true."
-
-    output = ""
-    for res in result:
-        tmpOutput = []
-        for var in res:
-            tmpOutput.append(var + " = " + format_value(res[var]))
-        output += ", ".join(tmpOutput) + " ;\n"
-    output = output[:-3] + " ."
-
-    return output
-#END FUNCION DE INTERNET PARA TRASPASAR ATOM'S
