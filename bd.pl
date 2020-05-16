152820510efa96f8ba254f88b36784d388a2e5c2
Matias <matias.coronado@usach.cl>
2020-05-16 01:03:32 -0400

    Base de conocimiento + ideas

---
+++ bd.pl
@@ -0,0 +1,26 @@
+sintoma(fiebre).
+sintoma(tos).
+sintoma(escalofrios).
+sintoma('no muerte').
+
+enfermedad('resfrio comun').
+enfermedad('resfrio mortal').
+
+poblacionRiesgo(adulto)
+poblacionRiesgo(infante)
+poblacionRiesgo('adulto mayor')
+
+relacion(enfermedad('resfrio comun'),sintoma(tos)).
+relacion(enfermedad('resfrio comun'),sintoma(fiebre)).
+relacion(enfermedad('resfrio comun'),sintoma('no muerte')).
+
+relacion(enfermedad('resfrio mortal'),sintoma(tos)).
+relacion(enfermedad('resfrio mortal'),sintoma(fiebre)).
+relacion(enfermedad('resfrio mortal'),sintoma(escalofrios)).
+
+significativa(enfermedad('resfrio mortal'),sintoma(muerte))
+significativa(enfermedad('resfrio comun'),sintoma('no muerte'))
+
+tratamiento(enfermedad('resfrio comun'),poblacionRiesgo(infante),'Ahhh se morira el cabro chico')
+tratamiento(enfermedad('resfrio comun'),poblacionRiesgo(adulto),'Ahhh se morira el adulto')
+tratamiento(enfermedad('resfrio comun'),poblacionRiesgo('adulto mayor'),'Ahhh se morira el adulto mayor')
