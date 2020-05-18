8895401097f7546a547cafe1e66f868d5aa57962
Matias <matias.coronado@usach.cl>
2020-05-18 19:56:51 -0400

    Sintomas + avance tratamiento

---
+++ tratamiento.pl
@@ -0,0 +1,17 @@
+tratamiento(enfermedad(asma),'
+Los tipos de medicamentos de alivio rapido son los siguientes:
+    *Agonistas beta de accion rapida, tales como salbutamol (ProAir HFA, Ventolin HFA y otros) o levalbuterol (Xopenex).
+    Los cuales deben inhalarse mediante una mascarilla o una boquilla, para situaciones de bajo riesgo.
+    *Ipratropio (Atrovent) para relajar de inmediato las vias respiratorias al tener sintomas de mediana gravedad.
+    *Corticoesteroides orales e intravenosos para tratar los sintomas intensos de asma.
+Siempre consulte al medico para evaular tratamientos a largo plazo.
+').
+tratamiento(enfermedad('neumonia'),'wqwdadaw').
+tratamiento(enfermedad('bronquitis'),'').
+tratamiento(enfermedad('resfrio comun'),'').
+tratamiento(enfermedad('renitis no alergica'),'').
+tratamiento(enfermedad(amigdalitis),'').
+tratamiento(enfermedad(paperas),'').
+tratamiento(enfermedad('herpes labial'),'').
+tratamiento(enfermedad('sinusitis cronica'),'').
+tratamiento(enfermedad('influenza aviar'),'').
