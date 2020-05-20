481804883df6d53734cf0752aec0e5831e7c3403
Franco <franco.tapia.c@usach.cl>
2020-05-19 22:35:31 -0400

    Enfermedades + relación síntoma/enfermedad
    - Se agregan las enfermedades
    - Se agregan las relaciones entre cada enfermedad con sus respectivos síntomas

---
+++ enfermedades.pl
@@ -0,0 +1,11 @@
+enfermedad(asma).
+enfermedad(neumonia).
+enfermedad(bronquitis).
+enfermedad('resfrio comun').
+enfermedad('renitis no alergica').
+enfermedad(amigdalitis).
+enfermedad(paperas).
+enfermedad('herpes labial').
+enfermedad('sinusitis cronica').
+enfermedad('influenza aviar').
+
